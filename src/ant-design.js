2d26d6067999d81b9dba029164f16fdf7a48155e
孟小成 <litchilab@foxmail.com>
2023-07-29 01:39:11 +0800

    初始样式

---
+++ src/ant-design.js
@@ -0,0 +1,9 @@
+export default {
+    token: {
+        colorPrimary: '#0035f7',
+        colorBgContainerDisabled:'#ffffff',
+        colorLink:'#0035f7',
+        colorLinkActive:'#0035f7',
+        colorLinkHover:'#0035f7'
+    },
+}
